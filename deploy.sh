3bfb58b1bef0208c9dd81d9291359e55b7b7d3c2
yangeee <534650291@qq.com>
2019-07-30 22:06:53 +0800

    修改介绍，增加Todo

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yangeee/YY-canvas.git master:gh-pages
+git push -f git@github.com:yangeee/Ninja-canvas.git master:gh-pages
 
 cd -
